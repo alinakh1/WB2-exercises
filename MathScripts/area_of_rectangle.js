8a1f7af9b02fd18e20085bba3eaed1153da7327f
alinakh1 <akhabibullina@nyc.yearup.org>
2022-10-18 09:56:19 -0400

    Added exercise

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,8 @@
+//inputs
+var width = 12;
+var height = 8;
+//calculations
+var area = width * height;
+
+//output
+console.log("Your area of the rectangle is " + area);
---
+++ MathScripts/net_worth.js
@@ -0,0 +1,10 @@
+//sample inputs
+var assets = 5000;
+var debts = 2000;
+
+//calculations
+var netWorth = assets - debts;
+//output
+//console.log("The total due is " + totalDue);
+
+console.log("Your networth is " + netWorth);
